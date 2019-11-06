5c7822b1551caa463ad657642b8c49d74c215e28
Tonggo Oscar <rafarareza@gmail.com>
2019-11-06 10:10:31 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora, nostrum! Ea cumque harum temporibus? Mollitia iure quae laudantium, velit, optio et inventore unde omnis id quas molestiae itaque consequuntur quisquam.</p>
+    <ul>
+    <li>satu</li>
+    <li>dua</li>
+    </ul>
 </body>
 </html>
